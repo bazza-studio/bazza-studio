a3890fcf2e7c587186e7dc4a00c251b5d4352ecf
タイラー <tylerymh27@gmail.com>
2025-02-10 00:28:23 -0300

    fix: reubicar el componente Cursor en el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-geist`}
       >
-        <Cursor />
         <Provider>{children}</Provider>
+        <Cursor />
       </body>
     </html>
   );
