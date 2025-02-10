7a7ae2f628b424fad4199057e8b3b6fa393a267f
タイラー <tylerymh27@gmail.com>
2025-02-10 00:30:55 -0300

    fix: eliminar el proveedor no utilizado en el layout y simplificar la configuración de Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Cursor } from "./components/cursor";
-import { Provider } from "react-wrap-balancer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-geist`}
       >
-        <Provider>{children}</Provider>
+        {children}
         <Cursor />
       </body>
     </html>
